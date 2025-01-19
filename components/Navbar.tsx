21e957216c001c7a900e122f1a9d7496579a2ca3
Adoosko <120666532+Adoosko@users.noreply.github.com>
2025-01-19 12:53:54 +0100

    added compare component to hero

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,15 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav
-      className="flexBetween bg-neutral-900
-  padding-container relative z-30 py-5 max-container"
-    >
-      <Link href={"/"}>
-        <Image src={"/logo.png"} alt="logo" width={100} height={100}></Image>
-      </Link>
-      {/* <ul className="hidden h-full gap-12 lg:flex">
+    <nav className="bg-neutral-900 ">
+      <div
+        className="flexBetween 
+  px-10 relative z-30 py-5 max-container"
+      >
+        <Link href={"/"}>
+          <Image src={"/logo.png"} alt="logo" width={100} height={100}></Image>
+        </Link>
+        {/* <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
@@ -24,22 +25,23 @@ const Navbar = () => {
           </Link>
         ))}
       </ul> */}
-      <div className="lg:flexCenter hidden">
-        <Button
-          type="button"
-          title="Kontakt"
-          icon="/user.svg"
-          variant="btn_green"
-        ></Button>
-      </div>
+        <div className="lg:flexCenter hidden">
+          <Button
+            type="button"
+            title="Kontakt"
+            icon="/user.svg"
+            variant="btn_green"
+          ></Button>
+        </div>
 
-      <Image
-        src={"/menu.svg"}
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer  lg:hidden"
-      ></Image>
+        <Image
+          src={"/menu.svg"}
+          alt="menu"
+          width={32}
+          height={32}
+          className="inline-block cursor-pointer  lg:hidden"
+        ></Image>
+      </div>
     </nav>
   );
 };
