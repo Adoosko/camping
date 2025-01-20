107acd72741a96f42a29b03acc43df6421e213dc
Adoosko <120666532+Adoosko@users.noreply.github.com>
2025-01-20 18:37:53 +0100

    try2

--- components/HeroHighlightDemo.tsx
+++ components/HeroHighlightDemo.tsx
@@ -1,13 +1,10 @@
 "use client";
-import { HeroHighlight } from "./ui/hero-highlight";
 
 export function HeroHighlightDemo() {
   return (
-    <HeroHighlight>
-      <h3 className=" px-4 bold-32 text-white lg:bold-52 max-w-4xl leading-relaxed lg:leading-snug text-center lg:text-left mx-auto ">
-        Unavuje ťa rozhodovať, čo jesť? Objav recepty, ktoré urobia chudnutie a
-        rast svalov jednoduché a chutné!
-      </h3>
-    </HeroHighlight>
+    <h3 className=" px-4 bold-32 text-white lg:bold-52 max-w-4xl leading-relaxed lg:leading-snug text-center lg:text-left mx-auto ">
+      Unavuje ťa rozhodovať, čo jesť? Objav recepty, ktoré urobia chudnutie a
+      rast svalov jednoduché a chutné!
+    </h3>
   );
 }
